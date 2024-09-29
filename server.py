cf1a72075c180d22ef90b284bfd82032987407ae
hammad-ibrahim <95659983+hammad-ibrahim@users.noreply.github.com>
2024-09-29 16:55:17 +0300

    Update server.py

--- server.py
+++ server.py
@@ -3,11 +3,6 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route("/")
-
 def hello():
-    return "<p> Hello From Home With Hammad </p>
-            <h1> Hello My firnd </h1> "
-    
-            
-    
- 
+    return """<p>Hello From Home With Hammad</p>
+              <h1>Hello My friend</h1>"""
